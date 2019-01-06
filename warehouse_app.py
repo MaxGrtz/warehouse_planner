c296ad658d318bc1c41dd47a83780d5852455b81
Maximilian Gartz <maximilian.gartz@inside-m2m.de>
2019-01-06 14:24:25 +0100

    added code for automatically installing requirements when running the application

--- warehouse_app.py
+++ warehouse_app.py
@@ -1,7 +1,18 @@
 from tkinter import Tk 
 import gui
+from subprocess import Popen, PIPE
+ 
 
+# try to install dependencies
+try:
+    process = Popen(['pip', 'install', '-r', 'requirements.txt'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+except:
+    print('tried to install dependencies but failed, please update your python and pip versions!')
+    print('alternatively ignore this message and install dependencies yourself')
+    print(stderr)
 
+# run application
 window = Tk()
 warehouse_gui = gui.Gui(window)
 window.mainloop()
