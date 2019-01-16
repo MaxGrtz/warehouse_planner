6365fc37d78558cbd87f7f7c47d0e0ad823e1d45
Maximilian Gartz <maximilian.gartz@inside-m2m.de>
2019-01-16 22:15:58 +0100

    fixed dependency installer

--- warehouse_app.py
+++ warehouse_app.py
@@ -1,17 +1,28 @@
-from tkinter import Tk 
-import gui
-from subprocess import Popen, PIPE
- 
 
 # try to install dependencies
+from subprocess import Popen, PIPE
+
+answer = input('Do you want to install dependencies automatically before starting? [y/n]: \n')
 try:
-    process = Popen(['pip', 'install', '-r', 'requirements.txt'], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
+    if (answer == 'y') or (answer == 'Y'):
+        print('checking dependencies...')
+        process = Popen(['pip', 'install', '-r', 'requirements.txt'], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout)
+        print(stderr)
+    else:
+        print('trying to start without checking dependencies...')
+        print('please install dependencies yourself (see requirements.txt) and restart if problems occur!')
+
 except:
-    print('tried to install dependencies but failed, please update your python and pip versions!')
-    print('alternatively ignore this message and install dependencies yourself')
+    print('tried to install dependencies but failed, please update your python (to 3.7x) and pip versions!')
+    print('alternatively ignore this message and install dependencies yourself (see requirements.txt)')
+
 
 # run application
+from tkinter import Tk 
+import gui
+
 window = Tk()
 warehouse_gui = gui.Gui(window)
 window.mainloop()
